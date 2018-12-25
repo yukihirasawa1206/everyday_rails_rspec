302cb0837f493d1074ae3f2f7f2f891726032fe1
yuki <h_x04yjr0@yahoo.co.jp>
2018-12-25 10:20:54 +0000

    Factory Bot の導入

--- config/application.rb
+++ config/application.rb
@@ -16,10 +16,10 @@ module Projects
     # -- all .rb files in that directory are automatically loaded.
     config.generators do |g|
       g.test_framework :rspec,
-      fixtures: false,
       view_specs: false,
       helper_specs: false,
-      routing_specs: false
+      routing_specs: false,
+      request_specs: false
     end
   end
 end
