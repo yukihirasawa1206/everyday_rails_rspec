462185fa662e89d8ac2827a0382f05282458e36c
yuki <h_x04yjr0@yahoo.co.jp>
2018-12-25 10:20:54 +0000

    Factory Bot の導入

---
+++ spec/controllers/home_controller_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe HomeController, type: :controller do
+
+  describe "#index" do
+    it "responds successfully" do
+      get :index
+      # expect(response).to be_success
+      expect(response).to have_http_status "200"
+    end
+  end
+  
+end
