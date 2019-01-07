7077982767dcfa6a2a1d562949c22f2d785ae65b
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-01-08 00:02:24 +0900

    リファクタリング等

--- spec/features/tasks_spec.rb
+++ spec/features/tasks_spec.rb
@@ -1,30 +1,49 @@
 require 'rails_helper'
 
 RSpec.feature "Tasks", type: :feature do
-  
-  scenario "user toggles a task", js: true do
-    user = FactoryBot.create(:user)
-    project = FactoryBot.create(
-    :project,
-    name: "RSpec tutorial",
-    owner: user)
-    task = project.tasks.create!(name: "Finish RSpec tutorial")
-    
-    visit root_path
-    click_link "Sign in"
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Log in"
-    
-    click_link "RSpec tutorial"
-    check "Finish RSpec tutorial"
-    
-    expect(page).to have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to be_completed
-    
-    uncheck "Finish RSpec tutorial"
-    
-    expect(page).to_not have_css "label#task_#{task.id}.completed"
-    expect(task.reload).to_not be_completed
-  end
+
+	let(:user){FactoryBot.create(:user)}
+	let(:project){
+		FactoryBot.create(:project, name: "RSpec tutorial", owner: user)
+	}
+	let(:task){project.tasks.create!(name: "Finish RSpec tutorial")}
+
+	scenario "user toggles a task", js: true do
+
+		sign_in user
+		go_to_project(project.name)
+
+		complete_task(task.name)
+		expect_complete_task(task.name)
+
+		undo_complete_task(task.name)
+		expect_incomplete_task(task.name)
+
+	end
+
+	def go_to_project(name)
+		visit  root_path
+		click_link name
+	end
+
+	def complete_task(name)
+		check name
+	end
+
+	def undo_complete_task(name)
+		uncheck name
+	end
+
+	def expect_complete_task(name)
+		aggregate_failures do
+			expect(page).to have_css "label.completed", text: name
+			expect(task.reload).to be_completed
+		end
+	end
+
+	def expect_incomplete_task(name)
+		expect(page).to_not have_css "label.completed", text:name
+		expect(task.reload).to_not be_completed
+	end
+
 end
--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -1,49 +1,50 @@
 require 'rails_helper'
 
 RSpec.describe Note, type: :model do
-  
-  it "is valid with a user, project and message" do
-    note = FactoryBot.build(:note)
-    expect(note).to be_valid
-  end
-  
-  it "is invalid without a message" do
-    note = FactoryBot.build(:note, message: nil)
-    note.valid?
-    expect(note.errors[:message]).to include("can't be blank")
-  end
-  
-  describe "search message for a term" do
-    
-    context "when a match is found" do
-      
-      before do
-        @note1 = FactoryBot.create(:note, message: "This is first message.")
-        @note2 = FactoryBot.create(:note, message: "This is second message.")
-        @note3 = FactoryBot.create(:note, message: "First, preheat oven.")
-      end
-      it "returns notes that match the search term" do
-        expect(Note.search("first")).to include(@note1, @note3)
-        expect(Note.search("first")).to_not include(@note2)
-      end
-    end
-    
-    context "when no match is found" do
-      it "returns an empty collection" do
-        expect(Note.search("third")).to be_empty
-      end
-    end
-  end
-  
+
+	let(:user){FactoryBot.create(:user)}
+	let(:project){FactoryBot.create(:project, owner: user)}
+
+	it { is_expected.to have_attached_file(:attachment) }
+
+	it "is valid with a user, project and message" do
+		note = FactoryBot.build(:note)
+		expect(note).to be_valid
+	end
+
+	it "is invalid without a message" do
+		note = FactoryBot.build(:note, message: nil)
+		note.valid?
+		expect(note.errors[:message]).to include("can't be blank")
+	end
+
+	describe "search message for a term" do
+
+		context "when a match is found" do
+
+			before do
+				@note1 = FactoryBot.create(:note, message: "This is first message.")
+				@note2 = FactoryBot.create(:note, message: "This is second message.")
+				@note3 = FactoryBot.create(:note, message: "First, preheat oven.")
+			end
+			it "returns notes that match the search term" do
+				expect(Note.search("first")).to include(@note1, @note3)
+				expect(Note.search("first")).to_not include(@note2)
+			end
+		end
+
+		context "when no match is found" do
+			it "returns an empty collection" do
+				expect(Note.search("third")).to be_empty
+			end
+		end
+	end
+
+	it "delegates name to the user who created it" do
+		user = instance_double("User", name: "Fake User")
+		note = Note.new
+		allow(note).to receive(:user).and_return(user)
+		expect(note.user_name).to eq "Fake User"
+	end
+
 end
-
-
-
-
-
-
-
-
-
-
-
