fc533415bee88b3456afbbbee892175efe5a7744
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-01-07 09:06:51 +0900

    add request spec

---
+++ spec/requests/homes_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+RSpec.describe "Homes", type: :request do
+  describe "GET /homes" do
+
+    it "respondes successfully" do
+			get root_path
+			expect(response).to be_success
+			expect(response).to have_http_status "200"
+    end
+
+  end
+end
