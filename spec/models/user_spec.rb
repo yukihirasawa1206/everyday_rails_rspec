6523b5fe283b6bd5301e9d8289e5a0bf7cd8384d
yukihirasawa1206 <h_x04yjr0@yahoo.co.jp>
2019-01-08 16:09:12 +0900

    Web service test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,4 +9,19 @@ RSpec.describe User, type: :model do
 	subject(:user){FactoryBot.build(:user)}
 	it { is_expected.to satisfy{|user| user.name == "Aaron Sumner"} }
 
+	it "sends a welcome email on account creation" do
+		allow(UserMailer).to receive_message_chain(:welcome_email, :deliver_later)
+		user = FactoryBot.create(:user)
+		expect(UserMailer).to have_received(:welcome_email).with(user)
+	end
+
+	it "performs geocoding" do
+		user = FactoryBot.create(:user, last_sign_in_ip: "161.185.207.20")
+		expect {
+			user.geocode
+		}.to change(user, :location).
+		from(nil).
+		to("Brooklyn, New York, US")
+	end
+
 end
